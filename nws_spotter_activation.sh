13af5754e101e3cc93f55380129b58aeb533024d
Kevin P. Inscoe <kevin@inscoe.org>
2018-03-10 09:59:16 -0500

    Initial

---
+++ nws_spotter_activation.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+for WFO in MLB MRX
+do
+	echo "HWO statement for $WFO..."
+	echo " "
+	URL="https://forecast.weather.gov/product.php?site=NWS&issuedby=`echo $WFO`&product=HWO&format=txt&version=1&glossary=0"
+# SPOTTER INFORMATION STATEMENT
+	#curl -s "$URL" | html2text | grep -i -A 4 "Spotter activation" 
+	#curl -s "$URL" | html2text | grep -i -A 7 "SPOTTER INFORMATION STATEMENT" | sed -e 's/\$\$//g'
+	curl -s "$URL" | html2text | grep -i -A 10 "SPOTTER INFORMATION STATEMENT" | sed -e '/=========/q'
+	echo " "
+	echo "For full outlook click on $URL"
+	echo " "
+done
